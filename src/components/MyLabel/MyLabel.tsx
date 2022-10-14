0e4aff30d61060914ff2db3e6ddef70abbc3bc10
= <alain.moraga@openagents.io>
2022-10-13 23:54:54 -0300

    initial commit

---
+++ src/components/MyLabel/MyLabel.tsx
@@ -0,0 +1,40 @@
+import './MyLabel.css';
+
+export interface MyLabelProps {
+  /**
+   * Mensaje a mostrar en la etiqueta.
+   */
+  label: string;
+  /**
+   * Tamaño de la etiqueta.
+   */
+  size: 'normal' | 'h1' | 'h2' | 'h3';
+  /**
+   * Mensaje en mayusculas o minusculas.
+   */
+  allCaps: boolean;
+  /**
+   * Color del texto del mensaje.
+   */
+  color: 'primary' | 'secondary' | 'tertiary';
+  /**
+   * Color del texto del mensaje.
+   */
+  fontColor?: string;
+}
+
+export const MyLabel = ({
+  label = 'Por defecto',
+  size = 'h1',
+  color = 'primary',
+  allCaps = false,
+  fontColor,
+}: MyLabelProps) => {
+  return (
+    <div className={`label ${size} text-${color}`} style={{ color: fontColor }} >
+      {allCaps ? label.toUpperCase() : label}
+    </div>
+  );
+};
+
+export default MyLabel;
---
+++ src/stories/MyLabel.stories.tsx
@@ -0,0 +1,25 @@
+import { ComponentMeta, ComponentStory } from '@storybook/react';
+import MyLabel from '../components/MyLabel/MyLabel';
+
+export default {
+  title: 'UI/MyLabel',
+  component: MyLabel,
+  argTypes: {
+    color: { control: 'select' }
+  }
+} as ComponentMeta<typeof MyLabel>;
+
+
+const Template: ComponentStory<typeof MyLabel> = (args) => <MyLabel {...args} />
+
+export const Basic = Template.bind({});
+Basic.args = {
+  size: 'h1',
+  color: 'primary'
+}
+
+export const Test = Template.bind({});
+Test.args = {
+  size: 'h1',
+  color: 'secondary'
+}
